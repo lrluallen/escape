76ef32144c685130a4a48af9b8cd8b338384b238
BowerB <brandonkbower@gmail.com>
2020-04-27 18:02:59 -0700

    left control now toggles mouse on/off for interacting with ui, re-enabled canvas in scene

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -6,11 +6,15 @@ public class MouseLook : MonoBehaviour
 {
     float mouseSensitivity = 100f;
     public Transform playerBody;
-    float xRotation = 0f; 
+    float xRotation = 0f;
+    bool state;
+    bool press;
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked; 
+        Cursor.lockState = CursorLockMode.Locked;
+        state = true;
+        press = true;
         
     }
 
@@ -20,10 +24,24 @@ public class MouseLook : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f); 
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f); 
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
-        
+
+        if (Input.GetKeyDown(KeyCode.LeftControl) && press)
+        {
+            if (state)
+                Cursor.lockState = CursorLockMode.None;
+            else
+                Cursor.lockState = CursorLockMode.Locked;
+            state = !state;
+            press = false;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            press = true;
+        }
     }
 }
