c6703d7dc553fb4c78535af187b915901ae65e17
BowerB <brandonkbower@gmail.com>
2020-05-23 17:28:09 -0700

    Created General Monologue script

---
+++ Assets/Scripts/GeneralMonologue.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GeneralMonologue : MonoBehaviour
+{
+    public Text monologueTextField;
+    public bool persists;
+    //[TextArea(int minLines, int maxLines)];
+    [TextArea(10,15)]
+    public string text;
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            monologueTextField.text = text;
+        }
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if(!persists)
+                monologueTextField.text = "";
+        }
+
+    }
+}
