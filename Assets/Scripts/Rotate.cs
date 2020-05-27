8c21168f9b1024cca2fa3bd47533ee02a6aa367d
BowerB <brandonkbower@gmail.com>
2020-05-26 19:24:40 -0700

    Moved sprites folder out of scripts folder. Added Hand Models. Added Shark Prefab and Model. Added Rotation Script

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float rate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Rotate(0, rate * Time.deltaTime, 0); //rotates 50 degrees per second around z axis
+    }
+}
