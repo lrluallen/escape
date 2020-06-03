0676db8dacbcf753bf74dc01c4adf6b52e06c1eb
Lindsay Luallen <lluallen@uoregon.edu>
2020-06-03 06:52:01 -0700

    Pause Menu added, and text scaled to constant pixel size

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI; 
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (GameIsPaused) // game is paused, so call resume
+            {
+                Resume();
+            }
+            else // Game is not paused yet, so call pause
+            {
+                Pause();
+            }
+        }
+
+
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);// take away oause menu
+        Time.timeScale = 1f; // resume game time
+        GameIsPaused = false; // take away pause menu UI
+
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);// show pause menu
+        Time.timeScale = 0f; // freeze gametime
+        GameIsPaused = true; // show game is paused
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game Now... So long, and thanks for all the fish!");
+        Application.Quit();
+    }
+
+}
